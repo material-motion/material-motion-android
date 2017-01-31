3f8f22d15427599bf7150d8ae570fb5200a56bf6
Mark Wei <markwei@google.com>
2017-01-30 19:54:12 -0500

    rubberBanded() operator.
    Reviewers: O2 Material Motion, O6 Material Android platform reviewers, #material_motion, featherless
    
    Reviewed By: O2 Material Motion, #material_motion, featherless
    
    Subscribers: featherless
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D2617

--- sample/src/main/java/com/google/android/material/motion/streams/sample/GesturesActivity.java
+++ sample/src/main/java/com/google/android/material/motion/streams/sample/GesturesActivity.java
@@ -1,5 +1,6 @@
 package com.google.android.material.motion.streams.sample;
 
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,6 +11,7 @@ import com.google.android.material.motion.streams.interactions.Pinchable;
 import com.google.android.material.motion.streams.interactions.Rotatable;
 
 import static com.google.android.material.motion.streams.operators.FloatArrayOperators.lockToYAxis;
+import static com.google.android.material.motion.streams.operators.FloatArrayOperators.rubberBanded;
 
 public class GesturesActivity extends AppCompatActivity {
 
@@ -23,7 +25,9 @@ public class GesturesActivity extends AppCompatActivity {
     View target = findViewById(R.id.target);
 
     Draggable draggable = new Draggable();
-    draggable.compose(lockToYAxis(0f));
+    draggable
+      .compose(lockToYAxis(0f))
+      .compose(rubberBanded(new RectF(-500f, -500f, 500f, 500f), 200f));
 
     runtime.addInteraction(draggable, target);
     runtime.addInteraction(new Pinchable(), target);
