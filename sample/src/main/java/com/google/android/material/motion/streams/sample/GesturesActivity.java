e4d90f6268c26f122ef39628b3e691c22ff40e4b
Mark Wei <markwei@google.com>
2017-01-30 19:38:42 -0500

    Formalize lockToYAxis() as an operator.

--- sample/src/main/java/com/google/android/material/motion/streams/sample/GesturesActivity.java
+++ sample/src/main/java/com/google/android/material/motion/streams/sample/GesturesActivity.java
@@ -4,11 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.google.android.material.motion.streams.MotionObservable.MapOperation;
 import com.google.android.material.motion.streams.MotionRuntime;
 import com.google.android.material.motion.streams.interactions.Draggable;
 import com.google.android.material.motion.streams.interactions.Pinchable;
 import com.google.android.material.motion.streams.interactions.Rotatable;
+
+import static com.google.android.material.motion.streams.operators.FloatArrayOperators.lockToYAxis;
 
 public class GesturesActivity extends AppCompatActivity {
 
@@ -22,13 +23,7 @@ public class GesturesActivity extends AppCompatActivity {
     View target = findViewById(R.id.target);
 
     Draggable draggable = new Draggable();
-    draggable.compose(new MapOperation<Float[], Float[]>() {
-      @Override
-      public Float[] transform(Float[] value) {
-        Float[] yOnly = new Float[]{0f, value[1]};
-        return yOnly;
-      }
-    });
+    draggable.compose(lockToYAxis(0f));
 
     runtime.addInteraction(draggable, target);
     runtime.addInteraction(new Pinchable(), target);
