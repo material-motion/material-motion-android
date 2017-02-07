19e565fb4adb6bfdaa69c3aaaeacb062e2147cf6
Mark Wei <markwei@google.com>
2017-02-06 19:58:38 -0800

    Add CheckboardDrawable to demo.
    Reviewers: O2 Material Motion, O6 Material Android platform reviewers, #material_motion, featherless
    
    Reviewed By: O2 Material Motion, #material_motion, featherless
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D2659

---
+++ sample/src/main/java/com/google/android/material/motion/streams/sample/CheckerboardDrawable.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2016-present The Material Motion Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.android.material.motion.streams.sample;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+
+/**
+ * Draws a checkerboard pattern.
+ */
+public class CheckerboardDrawable extends Drawable {
+  public static final int COLS = 10;
+  public static final int ROWS = 10;
+  private final Paint gridPaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.DITHER_FLAG);
+  private final Paint backgroundPaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.DITHER_FLAG);
+
+  public CheckerboardDrawable() {
+    gridPaint.setColor(Color.BLACK);
+    backgroundPaint.setColor(Color.RED);
+  }
+
+  @Override
+  public void draw(Canvas canvas) {
+    Rect bounds = getBounds();
+
+    canvas.drawRect(bounds, backgroundPaint);
+
+    float cellWidth = (float) bounds.width() / COLS;
+    float cellHeight = (float) bounds.height() / ROWS;
+
+    for (int i = 0, x = bounds.left; i < COLS + 1; i++, x += cellWidth) {
+      if (i == 0 || i == COLS) {
+        gridPaint.setStrokeWidth(10);
+      } else {
+        gridPaint.setStrokeWidth(1);
+      }
+      canvas.drawLine(x, bounds.top, x, bounds.bottom, gridPaint);
+    }
+    for (int i = 0, y = bounds.top; i < ROWS + 1; i++, y += cellHeight) {
+      if (i == 0 || i == COLS) {
+        gridPaint.setStrokeWidth(10);
+      } else {
+        gridPaint.setStrokeWidth(1);
+      }
+      canvas.drawLine(bounds.left, y, bounds.right, y, gridPaint);
+    }
+  }
+
+  @Override
+  public void setAlpha(int alpha) {
+    gridPaint.setAlpha(alpha);
+    invalidateSelf();
+  }
+
+  @Override
+  public void setColorFilter(ColorFilter colorFilter) {
+    gridPaint.setColorFilter(colorFilter);
+    invalidateSelf();
+  }
+
+  @Override
+  public int getOpacity() {
+    return PixelFormat.TRANSLUCENT;
+  }
+}
