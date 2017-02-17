9274bbfd934e5779f005e816974240255ad1cf9f
Mark Wei <markwei@google.com>
2017-02-16 18:46:34 -0800

    Add gif view to sample.
    Clean up sample.

--- sample/src/main/java/com/google/android/material/motion/streams/sample/StreamsActivity.java
+++ sample/src/main/java/com/google/android/material/motion/streams/sample/StreamsActivity.java
@@ -15,61 +15,26 @@
  */
 package com.google.android.material.motion.streams.sample;
 
-import android.graphics.Color;
-import android.graphics.PointF;
-import android.graphics.Typeface;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.style.StyleSpan;
-import android.util.Property;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-import com.google.android.material.motion.gestures.DragGestureRecognizer;
-import com.google.android.material.motion.observable.IndefiniteObservable;
-import com.google.android.material.motion.observable.IndefiniteObservable.Subscription;
-import com.google.android.material.motion.streams.MotionObservable;
-import com.google.android.material.motion.streams.MotionObservable.FilterOperation;
-import com.google.android.material.motion.streams.MotionObservable.MapOperation;
 import com.google.android.material.motion.streams.MotionObservable.MotionObserver;
-import com.google.android.material.motion.streams.MotionObservable.MotionState;
 import com.google.android.material.motion.streams.MotionRuntime;
-import com.google.android.material.motion.streams.ReactiveWritable;
-import com.google.android.material.motion.streams.properties.ViewProperties;
-import com.google.android.material.motion.streams.sources.GestureSource;
-import com.google.android.material.motion.streams.springs.LabVectorizer;
+import com.google.android.material.motion.streams.springs.FloatVectorizer;
 import com.google.android.material.motion.streams.springs.MaterialSpring;
-import com.google.android.material.motion.streams.springs.RgbVectorizer;
-
-import java.util.Locale;
-
-import static com.google.android.material.motion.streams.MotionObservable.ACTIVE;
-import static com.google.android.material.motion.streams.MotionObservable.AT_REST;
-import static com.google.android.material.motion.streams.operators.GestureOperators.centroid;
 
 /**
  * Streams for Android sample Activity.
  */
 public class StreamsActivity extends AppCompatActivity {
 
-  private static final String[] values = new String[]{
-    "foo", "skip", "bar", "baz", "qux"
-  };
-
   private final MotionRuntime runtime = new MotionRuntime();
 
-  private TextView text;
-  private Button nextButton;
-  private Button unsubscribeButton;
-
-  private View dragTarget;
-
+  private View tweenTarget;
   private View springTarget;
 
   private MotionObserver<String> callback;
@@ -81,135 +46,19 @@ public class StreamsActivity extends AppCompatActivity {
 
     setContentView(R.layout.streams_activity);
 
-    text = (TextView) findViewById(R.id.text);
-    nextButton = (Button) findViewById(R.id.next_button);
-    unsubscribeButton = (Button) findViewById(R.id.unsubscribe_button);
-
-    dragTarget = findViewById(R.id.drag_target);
-
+    tweenTarget = findViewById(R.id.tween_target);
     springTarget = findViewById(R.id.spring_target);
-    runDemo1();
-    runDemo2();
-    runDemo3();
+
+    runDemo();
   }
 
-  private void runDemo1() {
-    final MotionObservable<String> observable = new MotionObservable<>(
-      new IndefiniteObservable.Connector<MotionObserver<String>>() {
-
-        @NonNull
-        @Override
-        public IndefiniteObservable.Disconnector connect(MotionObserver<String> observer) {
-          registerButtonCallback(observer);
-          return new IndefiniteObservable.Disconnector() {
-
-            @Override
-            public void disconnect() {
-              unregisterButtonCallback();
-            }
-          };
-        }
-      });
-
-    MotionObservable<CharSequence> stream = observable
-      .compose(new FilterOperation<String>() {
-
-        @Override
-        public boolean filter(String value) {
-          return !"skip".equals(value);
-        }
-      })
-      .compose(new MapOperation<String, CharSequence>() {
-
-        @Override
-        public CharSequence transform(String value) {
-          return italicizeAndCapitalize(value);
-        }
-      });
-
-    runtime.write(stream, text, TEXT_PROPERTY);
-
-    final Subscription subscription = stream.subscribe(
-      new MotionObserver<CharSequence>() {
-
-        @Override
-        public void next(CharSequence value) {
-          // No-op. Handled by write() above.
-        }
-
-        @Override
-        public void state(@MotionState int state) {
-          switch (state) {
-            case AT_REST:
-              text.setTextColor(Color.BLACK);
-              break;
-            case ACTIVE:
-              text.setTextColor(Color.RED);
-              break;
-          }
-        }
-      });
-
-    nextButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        if (callback != null) {
-          if (index < values.length) {
-            callback.next(values[index]);
-          }
-
-          if (index + 1 < values.length) {
-            callback.state(ACTIVE);
-          } else {
-            callback.state(AT_REST);
-          }
-
-          index++;
-        }
-      }
-    });
-
-    unsubscribeButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        subscription.unsubscribe();
-      }
-    });
-  }
-
-  private void runDemo2() {
-    DragGestureRecognizer gesture = new DragGestureRecognizer();
-    dragTarget.setOnTouchListener(gesture);
-
-    MotionObservable<PointF> observable =
-      GestureSource
-        .from(gesture)
-        .compose(centroid());
-
-    runtime.write(observable, new ReactiveWritable<PointF>() {
-      @Override
-      public void write(PointF value) {
-        text.setText(String.format(Locale.getDefault(), "[%f, %f]", value.x, value.y));
-      }
-    });
-  }
-
-  private void runDemo3() {
-    final MaterialSpring<View, Integer> rgbSpring = new MaterialSpring<>(
-      ViewProperties.BACKGROUND_COLOR,
-      new RgbVectorizer(),
-      Color.RED,
-      Color.RED,
-      0,
-      0.01f,
-      1,
-      10);
-    final MaterialSpring<View, Integer> labSpring = new MaterialSpring<>(
-      ViewProperties.BACKGROUND_COLOR,
-      new LabVectorizer(),
-      Color.RED,
-      Color.RED,
-      0,
+  private void runDemo() {
+    final MaterialSpring<View, Float> spring = new MaterialSpring<>(
+      View.TRANSLATION_X,
+      new FloatVectorizer(),
+      0f,
+      0f,
+      0f,
       0.01f,
       1,
       10);
@@ -221,13 +70,13 @@ public class StreamsActivity extends AppCompatActivity {
 
         switch (action) {
           case MotionEvent.ACTION_DOWN:
-            rgbSpring.destination.write(Color.GREEN);
-            labSpring.destination.write(Color.GREEN);
+            spring.destination.write(650f);
+            ObjectAnimator.ofFloat(tweenTarget, View.ALPHA, 0f).start();
             break;
           case MotionEvent.ACTION_UP:
           case MotionEvent.ACTION_CANCEL:
-            rgbSpring.destination.write(Color.RED);
-            labSpring.destination.write(Color.RED);
+            spring.destination.write(0f);
+            ObjectAnimator.ofFloat(tweenTarget, View.ALPHA, 1f).start();
             break;
         }
 
@@ -235,34 +84,6 @@ public class StreamsActivity extends AppCompatActivity {
       }
     });
 
-    runtime.addInteraction(rgbSpring, springTarget);
-    runtime.addInteraction(labSpring, dragTarget);
+    runtime.addInteraction(spring, springTarget);
   }
-
-  private CharSequence italicizeAndCapitalize(String value) {
-    Spannable spannable = new SpannableString(value.toUpperCase());
-    spannable.setSpan(new StyleSpan(Typeface.ITALIC), 0, spannable.length(), 0);
-    return spannable;
-  }
-
-  private void registerButtonCallback(MotionObserver<String> observer) {
-    callback = observer;
-  }
-
-  private void unregisterButtonCallback() {
-    callback = null;
-  }
-
-  private static Property<TextView, CharSequence> TEXT_PROPERTY = new Property<TextView, CharSequence>(CharSequence.class, "text") {
-
-    @Override
-    public CharSequence get(TextView object) {
-      return object.getText();
-    }
-
-    @Override
-    public void set(TextView object, CharSequence value) {
-      object.setText(value);
-    }
-  };
 }
