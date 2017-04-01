cd2ac44719a8763ff6c789c1c1d45c38f11ceb01
Mark Wei <markwei@google.com>
2017-03-31 17:54:40 -0700

    Rename package to material motion.
    Summary: Fixes https://github.com/material-motion/material-motion-android/issues/38
    
    Reviewers: O2 Material Motion, O6 Material Android platform reviewers, #material_motion, featherless
    
    Reviewed By: O2 Material Motion, #material_motion, featherless
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D2995

---
+++ library/src/main/java/com/google/android/material/motion/interactions/DirectlyManipulable.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2017-present The Material Motion Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.android.material.motion.interactions;
+
+import android.view.View;
+
+import com.google.android.material.motion.MotionRuntime;
+import com.google.android.material.motion.Interaction;
+
+public class DirectlyManipulable extends Interaction<View, Void> {
+
+  public final Draggable draggable = new Draggable();
+  public final Pinchable pinchable = new Pinchable();
+  public final Rotatable rotatable = new Rotatable();
+  public final AnchorPointAdjustable anchorPointAdjustable =
+    new AnchorPointAdjustable(pinchable, rotatable);
+
+  @Override
+  public void apply(MotionRuntime runtime, View target) {
+    runtime.addInteraction(draggable, target);
+    runtime.addInteraction(pinchable, target);
+    runtime.addInteraction(rotatable, target);
+    runtime.addInteraction(anchorPointAdjustable, target);
+  }
+}
