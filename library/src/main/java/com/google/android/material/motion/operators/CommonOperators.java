86deb164c76cd9710e246e3e78b4e10a65b8f52d
Mark Wei <markwei@google.com>
2017-04-04 15:12:43 -0400

    Implement log operator
    Summary: Fixes https://github.com/material-motion/material-motion-android/issues/56
    
    Reviewers: O2 Material Motion, O6 Material Android platform reviewers, #material_motion
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D3009

--- library/src/main/java/com/google/android/material/motion/operators/CommonOperators.java
+++ library/src/main/java/com/google/android/material/motion/operators/CommonOperators.java
@@ -17,6 +17,7 @@ package com.google.android.material.motion.operators;
 
 import android.support.annotation.Nullable;
 import android.support.annotation.VisibleForTesting;
+import android.util.Log;
 
 import com.google.android.indefinite.observable.Observer;
 import com.google.android.material.motion.Operation;
@@ -44,6 +45,24 @@ public class CommonOperators {
         lastValue = value;
         dispatched = true;
 
+        observer.next(value);
+      }
+    };
+  }
+
+  public static <T> Operation<T, T> log(final String tag) {
+    return log(tag, "");
+  }
+
+  public static <T> Operation<T, T> log(final String tag, final String prefix) {
+    return log(Log.DEBUG, tag, prefix);
+  }
+
+  public static <T> Operation<T, T> log(final int priority, final String tag, final String prefix) {
+    return new Operation<T, T>() {
+      @Override
+      public void next(Observer<T> observer, T value) {
+        Log.println(priority, tag, prefix + value);
         observer.next(value);
       }
     };
