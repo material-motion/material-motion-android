d75588385f53371730e39d1f6a5a691ee098a0e2
Mark Wei <markwei@google.com>
2017-04-05 17:47:48 -0400

    Implement offsetBy and scaledBy operators.
    Fixes https://github.com/material-motion/material-motion-android/issues/60

--- library/src/main/java/com/google/android/material/motion/operators/FloatOperators.java
+++ library/src/main/java/com/google/android/material/motion/operators/FloatOperators.java
@@ -12,12 +12,25 @@ public final class FloatOperators {
     throw new UnsupportedOperationException();
   }
 
-  public static Operation<Float, Float> normalizedBy(final float normal) {
+  public static Operation<Float, Float> offsetBy(final float offset) {
     return new MapOperation<Float, Float>() {
       @Override
       public Float transform(Float value) {
-        return value / normal;
+        return value + offset;
       }
     };
+  }
+
+  public static Operation<Float, Float> scaledBy(final float scale) {
+    return new MapOperation<Float, Float>() {
+      @Override
+      public Float transform(Float value) {
+        return value * scale;
+      }
+    };
+  }
+
+  public static Operation<Float, Float> normalizedBy(final float normal) {
+    return scaledBy(1 / normal);
   }
 }
